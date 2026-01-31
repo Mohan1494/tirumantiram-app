4b13a3e8d844f10f659d4b593cfb24eb5da87169
Mohan1494 <mohanbalakrishnan1494@gmail.com>
2026-01-31 11:05:12 +0530

    Fix typo in BASE_URL for backend connection

--- src/pages/Ask.js
+++ src/pages/Ask.js
@@ -6,7 +6,7 @@ function Ask() {
   const chatEndRef = useRef(null);
 
   
-   const BASE_URL = "https://mohan1494-tirumantiram-backend.hf.spac";
+   const BASE_URL = "https://mohan1494-tirumantiram-backend.hf.space";
   useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
